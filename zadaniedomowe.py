847d126acf0e52845e886b55f7ec177ad1c933ae
stanislawwmadej-arch <stanislaw.w.madej@gmail.com>
2026-07-16 17:29:05 +0200

    Add files via upload

---
+++ zadaniedomowe.py
@@ -0,0 +1,9 @@
+#na pierwszej lekcji nie bylo o tym jak pobrac info od osoby
+imie = "Stanisław"
+rok_urodzenia = 2006
+wiekE = 65
+rok = 2026
+wiek = rok - rok_urodzenia
+emerytura = wiekE - wiek
+wiadomosc = f"czesc {imie}. Obecnie masz {wiek} lat, więc do emerytury zostało Ci {emerytura} lat."
+print(wiadomosc)
