3a1c10d6f8c00d46520c6cb12ee0fbc66196ebe8
stanislawwmadej-arch <stanislaw.w.madej@gmail.com>
2026-07-18 14:20:31 +0200

    Add files via upload

--- zadaniedomowe.py
+++ zadaniedomowe.py
@@ -1,9 +1,11 @@
-#na pierwszej lekcji nie bylo o tym jak pobrac info od osoby
-imie = "Stanisław"
-rok_urodzenia = 2006
+imie = input("Podaj swoje imię: ")
+rok_urodzenia = int(input("Podaj swój rok urodzenia: "))
+
 wiekE = 65
 rok = 2026
+
 wiek = rok - rok_urodzenia
 emerytura = wiekE - wiek
+
 wiadomosc = f"czesc {imie}. Obecnie masz {wiek} lat, więc do emerytury zostało Ci {emerytura} lat."
 print(wiadomosc)
